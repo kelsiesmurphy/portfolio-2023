666e425b76a10ddd625866b1136ca036744eb22c
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2023-01-30 11:15:17 +0000

    Adding unread and pocketlint work

--- src/data/Projects.jsx
+++ src/data/Projects.jsx
@@ -1,7 +1,8 @@
 const Projects = [
 	{ id: 2, name: "AdventureGrid", image: '/portfolio-images/adventuregrid.png', github: 'https://kelsiesmurphy.pythonanywhere.com/'},
 	{ id: 1, name: "Surveyflow", image: '/portfolio-images/surveyflow.png', github: 'https://github.com/kelsiesmurphy/surveyflow'},
-	{ id: 3, name: "PocketLint", image: '/portfolio-images/pocketlint.png', github: ''}
+	{ id: 3, name: "Unread", image: '/portfolio-images/unread.png', github: 'https://github.com/kelsiesmurphy/unread'},
+	{ id: 4, name: "PocketLint", image: '/portfolio-images/pocketlint.png', github: 'https://github.com/kelsiesmurphy/pocketlint'}
 ]
 
 export default Projects;
