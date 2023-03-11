2e69d4d350c2c76fdd3326962f414f15f933aab1
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2023-03-11 20:13:58 +0000

    adding pickup to portfolio

--- src/data/Projects.jsx
+++ src/data/Projects.jsx
@@ -1,6 +1,18 @@
 const Projects = [
 	{ 
 		id: 1, 
+		name: "Pickup", 
+		image: '/portfolio-images/pickup.webp', 
+		github: 'https://github.com/kelsiesmurphy/pickup',
+		demo: 'https://www.joinpickup.co/',
+		code: '/code-snippets/pickup_code.webp',
+		projectType: 'Group Capstone CodeClan Project',
+		description: 'Pickup is a community network åfor organising litter picking events and allow the users to track the litter collected in a leaderboard format.',
+		furtherDetails: 'It is a web application with the front-end built in React, the back-end built in Java Spring, the hosting on Vercel and Neon/Digital Ocean respectively, and we even have an AWS S3 bucket set up for image storage. The authentication in the app is handled by Auth0.',
+		reflections: 'I built this app with two CodeClan classmates, Kyle Fenlon and Josh Montgomery, for our capstone project, and I learned so much building this full stack app, from the hosting to the adoption of TailwindCSS for the styling. The back-end has some really useful and clever RESTful endpoints to hit to get statistics, and we can load hundreds of thousands of litter items in our database. I spent most of my time in the front-end, but built out the data loading part of the backend, where it fills the database with dummy data, pulling from two different APIs to add random user and location data.'
+	},
+	{ 
+		id: 2, 
 		name: "AdventureGrid", 
 		image: '/portfolio-images/adventuregrid.webp', 
 		github: 'https://github.com/kelsiesmurphy/adventuregrid',
@@ -12,7 +24,7 @@ const Projects = [
 		reflections: 'I really enjoyed the styling of this project, and thinking through the user experience carefully. One of the key difficulties I encountered was actually the hosting of the demonstration site, as I had never hosted a Flask site before. I will return to the project in the future to add the e-commerce functionality. The code I am featuring here is the dynamic admin navigation, and how it reflects the current page by slightly adjusting its styling based on the page you are on, using Flask.'
 	},
 	{ 
-		id: 2, 
+		id: 3, 
 		name: "Surveyflow", 
 		image: '/portfolio-images/surveyflow.webp', 
 		github: 'https://github.com/kelsiesmurphy/surveyflow',
@@ -24,7 +36,7 @@ const Projects = [
 		reflections: 'My personal goals for this project were to try out a different tech stack to one I was used to. I chose Next.js and Typescript as they are becoming popular within the industry and I personally believe they will become industry standards within the next few years. My next step for this project would be to implement the subscription functionality with Stripe, and market it to potential users.'
 	},
 	{ 
-		id: 3, 
+		id: 4, 
 		name: "Unread", 
 		image: '/portfolio-images/unread.webp', 
 		github: 'https://github.com/kelsiesmurphy/unread',
@@ -36,7 +48,7 @@ const Projects = [
 		reflections: 'I believe we achieved a great deal as a team within the timeframe, and there were several opportunities to mentor other members of the team and be mentored back. In a future project, I intend to focus more closely on the testing aspect of the project. The code I have featured here is the API connection, this took the best part of a day to implement fully.'
 	},
 	{ 
-		id: 4, 
+		id: 5, 
 		name: "PocketLint", 
 		image: '/portfolio-images/pocketlint.webp', 
 		github: 'https://github.com/kelsiesmurphy/pocketlint',
